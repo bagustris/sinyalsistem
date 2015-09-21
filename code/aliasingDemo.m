2ddd4ffa2b3a28ed13ff603068b1ed13cde215f4
bagustris <bagustris@linuxmail.org>
2015-09-21 09:36:47 +0700

    modified:   code/aliasingDemo.m

--- code/aliasingDemo.m
+++ code/aliasingDemo.m
@@ -1,12 +1,13 @@
 % start with a continuous-time cosine signal at 60 Hz.
 % from: http://blogs.mathworks.com/steve/2010/03/03/aliasing-and-a-sampled-cosine-signal/
 
+% Signal spesification
 f = 60;  % Hz
 tmin = -0.05;
 tmax = 0.05;
 t = linspace(tmin, tmax, 400);
 x_c = cos(2*pi*f * t);
-plot(t,x_c)
+figure(1); plot(t,x_c)
 xlabel('t (seconds)')
 
 % Let's sample with a sampling frequency of 800 Hz. 
@@ -15,17 +16,22 @@ nmin = ceil(tmin / T);
 nmax = floor(tmax / T);
 n = nmin:nmax;
 x1 = cos(2*pi*f * n*T);
+figure(2); 
+subplot(411);
+plot(t,x_c)
 hold on
 plot(n*T,x1,'.')
 hold off
 
+% with 400 Hz
 T = 1/400;
 nmin = ceil(tmin / T);
 nmax = floor(tmax / T);
 n = nmin:nmax;
 x1 = cos(2*pi*f * n*T);
+subplot(412); 
 plot(t, x_c)
-hold on
+hold on;
 plot(n*T, x1, '.')
 hold off
 
@@ -35,40 +41,35 @@ nmin = ceil(tmin / T);
 nmax = floor(tmax / T);
 n = nmin:nmax;
 x1 = cos(2*pi*f * n*T);
+subplot(413)
 plot(t, x_c)
 hold on
 plot(n*T, x1, '.')
 hold off
 
+% with 120 Hz
 T = 1/120;
 nmin = ceil(tmin / T);
 nmax = floor(tmax / T);
 n = nmin:nmax;
 x1 = cos(2*pi*f * n*T);
+subplot(414)
 plot(t, x_c)
 hold on
 plot(n*T, x1, 'o')
 hold off
 
-% with 70 Hz
+% Sampled with 70 Hz -- > aliasing (undersampled)
 T = 1/70;
 nmin = ceil(tmin / T);
 nmax = floor(tmax / T);
 n = nmin:nmax;
 x1 = cos(2*pi*f * n*T);
+figure(3)
 plot(t, x_c)
 hold on
 plot(n*T, x1, 'o')
-hold off
-
-% Now look
-T = 1/70;
+hold on
 x_c = cos(2*pi*10 * t);
-nmin = ceil(tmin / T);
-nmax = floor(tmax / T);
-n = nmin:nmax;
-x1 = cos(2*pi*f * n*T);
-plot(t, x_c)
-hold on
-plot(n*T, x1, 'o')
+plot(t, x_c, '-r')
 hold off
