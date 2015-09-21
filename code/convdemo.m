421017b5a1129debe7cde0c75874b2ab56875865
bagustris <bagustris@linuxmail.org>
2015-09-21 14:34:21 +0700

    modified:   code/convdemo.m 	modified:   prakt/p1/praktikum1.lyx 	new file:   prakt/p1/praktikum1d.m

--- code/convdemo.m
+++ code/convdemo.m
@@ -1,6 +1,8 @@
+% convDemo2.m : Demo of convolution
 x = rand(1,10); % A random signal of length 10
 h = [0.5 0.5]; % An impulse response
 y = conv(x, h);% Convolution operation
+
 subplot(311)
 stem(0:length(x)-1, x,'*')
 axis([-1 length(y) 0 1])
---
+++ prakt/p1/praktikum1d.m
@@ -0,0 +1,26 @@
+% praktikum 1d.m
+% sistem: y(n) − y(n − 1) + 0.9y(n − 2) = x(n)
+% tentukan: impulse response, step response, stabil/tidak
+
+% Berdasarkan persamaan sistem maka koesien pers. beda
+
+addpath('../../code')
+b = [1];            % num
+a=[1, -1, 0.9];     % denum
+n = [-20:120];      % time interval
+
+% impulse response
+h = impz(b,a,n);
+subplot(2,1,1); stem(n,h);
+title('Impulse Response'); xlabel('n'); ylabel('h(n)')
+
+% step response
+x = stepseq(0,-20,120);
+s = filter(b,a,x);
+subplot(2,1,2); stem(n,s)
+title('Step Response'); xlabel('n'); ylabel('s(n)')
+
+% stability
+stable=sum(abs(h))
+z=roots(a);
+magz=abs(z)
