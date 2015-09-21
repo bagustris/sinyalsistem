2f3d49a66bee935acff6b132ceb1014da59682b6
bagustris <bagustris@linuxmail.org>
2015-09-21 14:40:02 +0700

    modified:   code/gema.m

--- code/gema.m
+++ code/gema.m
@@ -4,8 +4,9 @@
 
 
 [y, Fs]=wavread('../sinyal/fena_16k.wav'); % the signal is in y and sampling freq in Fs
-% sound(y,Fs); pause(10); % Play the original sound
-alpha = 0.9; D = 4196; % Echo parameters
-b = [1,zeros(1,D),alpha]; % Filter parameters
-x = filter(b,1,y); % Generate sound plus its echo
-sound(x,Fs); % Play sound with echo
+% sound(y,Fs); pause(10);   % Play the original sound
+alpha = 0.9;                % amplitude gema
+D = 4196;                   % Echo parameters
+b = [1,zeros(1,D),alpha];   % Filter parameters
+x = filter(b,1,y);          % Generate sound plus its echo
+sound(x,Fs);                % Play sound with echo
